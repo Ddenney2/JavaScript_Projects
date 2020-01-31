8396095c682f7e16fb1879f2dd0aab1dc5839aa2
Ddenney2 <60047265+Ddenney2@users.noreply.github.com>
2020-01-31 08:58:24 -0800

    Concatenating String
    Added another string

--- Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -5,3 +5,9 @@ document.write(X);
 
 var A = "This is a string";
 document.write(A);
+
+document.write("Lisa told Bart, \"Knock it off!\"");
+
+document.write("\"Be who you are and say what you feel,"
++ " beacuse those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
