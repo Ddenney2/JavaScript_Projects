5ef7914f9f4b31293b14641f3f5a786f0e714bba
Ddenney2 <60047265+Ddenney2@users.noreply.github.com>
2020-02-01 08:18:24 -0800

    Multiplication and Division Functions
    Added multiplication and division functions

--- Basic_JavaScript_Projects/JS/Basic_JavaScript_3.js
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_3.js
@@ -7,3 +7,13 @@ function subtraction_Function() {
     var Subtraction = 12 - 7;
     document.getElementById("Sub").innerHTML = "12 - 7 = " + Subtraction;
 }
+
+function multiplication() {
+    var simple_Math = 8 * 4;
+    document.getElementById("Multi").innerHTML = "8 * 4 = " + simple_Math;
+}
+
+function division() {
+    var simple_Math = 27 / 3;
+    document.getElementById("Divi").innerHTML = "27 / 3 = " + simple_Math;
+}
