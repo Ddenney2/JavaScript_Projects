020432ddc24b183782e631a01e85e18b4506f542
Ddenney2 <60047265+Ddenney2@users.noreply.github.com>
2020-02-01 13:40:24 -0800

    JavaScript_5

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_5.js
@@ -0,0 +1,51 @@
+document.write(typeof "Word");
+
+function my_Function() {
+    document.write(2E310);
+    document.getElementById("Pos").innerHTML;
+}
+
+function my_Function1() {
+    document.write(-3E310);
+    document.getElementById("Neg").innerHTML;
+}
+
+document.write(10 < 2);
+
+console.log(2 + 2);
+
+document.write("10" + 5);
+
+document.write(3 == 6);
+
+document.write(6 == 6);
+
+X = 82;
+Y = 82;
+document.write(X === Y);
+
+V = 6;
+K = 65;
+document.write(V === K);
+
+Word1 = 5;
+Doc1 = 5;
+document.write(Word1 === Doc1);
+
+Mammal1 = 7;
+Mammal2 = 1;
+document.write(Mammal1 === Mammal2)
+
+document.write(5 > 2 && 10 > 4);
+document.write(5 > 10 && 10 > 4);
+
+document.write(5 > 10 || 10 > 4);
+document.write(5 > 10 || 10 > 12);
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(5 > 10);
+}
+
+function not1_Function() {
+    document.getElementById("Not1").innerHTML = !(20 > 10);
+}
