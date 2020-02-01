6a1fdda5829e6c3016a98400731e4f2aab4c4864
Ddenney2 <60047265+Ddenney2@users.noreply.github.com>
2020-02-01 08:09:51 -0800

    Subtraction Function
    Added subtraction function

--- Basic_JavaScript_Projects/JS/Basic_JavaScript_3.js
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_3.js
@@ -2,3 +2,8 @@ function addition_Function() {
     var addition = 8 + 8;
     document.getElementById("Math").innerHTML = "8 + 8 = " + addition;
 }
+
+function subtraction_Function() {
+    var Subtraction = 12 - 7;
+    document.getElementById("Sub").innerHTML = "12 - 7 = " + Subtraction;
+}
