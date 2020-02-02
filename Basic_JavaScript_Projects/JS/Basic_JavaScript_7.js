f761e30d4645de6ccef8da4ad2268f858a95a1ca
Ddenney2 <60047265+Ddenney2@users.noreply.github.com>
2020-02-02 13:23:56 -0800

    JavaScript_7

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_7.js
@@ -0,0 +1,51 @@
+var X = 10;
+function Add_numbers_1() {
+    document.write(20 + X + "<br>");
+}
+funtion Add_numbers_2() {
+    document.write(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+function Add_numbers_3() {
+    var X = 10; /*Only works for Add_numbers_3 because dorectly below it. Won't work for Add_numbers_4 at same time.*/
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_4() {
+    document.write(X + 100);
+}
+Add_numbers_3();
+Add_numbers_4();
+
+function Add_numbers_5() {
+    var X = 10;
+    console.log(15 + X);
+}
+function Add_numbers_6() {
+    console.log(X + 100);
+}
+Add_numbers_5();
+Add_numbers_6();
+
+funtion get_Date() {
+    if (new Date().getHours() < 18) {
+        document.getElementById("Greeting").innerHTML = "How are you today?";
+    }
+    else {
+        Vote = "You are old enough vote!";
+    }
+    document.getElementById("How_old_are_you?").innerHTML = Vote;
+}
+
+
+
+function Age_Function() {
+    if (input < 50) {
+        document.getElementById("Ouch").innerHTML = "You may be headed over the hill.";
+    }
+    else {
+        Age = "You still have a ways to go.";
+    }
+    document.getElementById("Yay").innerHTML = Age;
+}
