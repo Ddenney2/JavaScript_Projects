805039b7f14354e3c60c0a32d93fd6dd71e643af
Ddenney2 <60047265+Ddenney2@users.noreply.github.com>
2020-02-03 09:12:42 -0800

    Functions
    Added working function and noted broken string method

--- Basic_JavaScript_Projects/JS/Basic_JavaScript_8.js
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_8.js
@@ -11,3 +11,20 @@ function slice_Method() {
     var Section = Sentence.slice(27,33);
     document.getElementById("Slice").innerHTML = Section;
 }
+function mySearch() {
+    var str = "I've got a lovely bunch of coconuts."
+    var A = str.search("lovely");
+    document.getElementById("bunch").innerHTML = A;
+  }
+
+/* This is where the code stops working. Getting this method to work is 
+getting confusing. I've tried many variations, but it is saying the class is not defined.
+Also corrupts the existing, working, functions above. */
+
+public class MyClass {
+    public static void main(String[] args) {
+        String txt = "Cookie eating monkeys.";
+        System.out.println(txt.toUpperCase());
+    
+    }
+  }
