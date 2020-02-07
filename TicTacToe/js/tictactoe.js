91087304e81206dc49aa30c12fd31ff960b48607
Ddenney2 <60047265+Ddenney2@users.noreply.github.com>
2020-02-07 08:11:32 -0800

    Functions
    Added more functions

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -178,9 +178,107 @@ function determineAvatar() {
     } else if (active == "Player 2") {
         var paintAvatar; //returned back the correct avatar
     }
+    return paintAvatar; //returned back the correct avatar
+}
+
+// this function changes active player over to the other player
+function avatarPlaced() {
+    var parseText = document.getElementById('gameMsg').innerHTML;
+    var showPlayer = document.getElementById('showPlayer'); //select the current element to memory
+    //check if there is already a winner. if there is, then don't continue the game
+    if (parseText == "That's three in a row, Player 1 wins!" | | parseText == "That's three in a row, Player 2 wins!") {
+        showPlayer.innerHTML = "Game Stopped";
+        showPlayer.style.color='red';
+    }
+    activePlayer = showPlayer.innerHTML; //get the current player from the element
+    if (activePlayer == "Player 1") {//once active player selects a square change the active player
+        showPlayer.innerHTML = "Player 2";
+    } else {
+        showPlayer.innerHTML = "Player 1";
+    }
+    check4Tie();//call this function to inquire if there was a tie
+}
+
+//this function will get the array of the current board
+//and check the proposed move for a validity
+function check(info,square) {
+    for (var i in info) {
+        var tempInfo = info[i].charAt(0); //comparing index of square
+        if (tempInfo == square) {
+            return tempInfo;
+        }
+    }
+}
+
+//as squares are selected they check in with this function to see if that particular
+//square has already been assigned and if it has not, record new square with the assigned avatar
+function recordMoves(square) {
+    var proposedMove = square;
+    var boardState = document.getElementById('boardState').innerHTML;
+    var info = boardState.split(','); //separate the string by commas to create an array
+    verdict = check(info,square); //call function to check if proposed square is already occupied
+    return verdict;
+}
 
 
+//this function will get list of previous moves
+//and then concatenate the current move to it.
+function recordMove(currentMove) {
+    var target = document.getElementById('boardState');
+    var previousMoves = target.innerHTML;
+    target.innerHTML = previousMoves+currentMove;
+}
 
+function checkForWinCon() {
+    var squareArray = [];
+    var target = document.getElementById('boardState');
+    var info = target.innerHTML; //raw array with squares and avatars
+    info = info.substring(1); //remove leading comma
+    info = info.split(','); //seperate the string by commas into an array
+    info.sort(); //sort the square array in order despite the actual gameplay sequence
+    for (var i in info) {
+        squareArray.push(info[i].charAt(0)); //new array with only squares not avatars
+    } 
+    //call this following array of functions to check for any of the possible win cons
+    checkWinCon1(info,squareArray);
+    checkWinCon2(info,squareArray);
+    checkWinCon3(info,squareArray);
+    checkWinCon4(info,squareArray);
+    checkWinCon5(info,squareArray);
+    checkWinCon6(info,squareArray);
+    checkWinCon7(info,squareArray);
+    checkWinCon8(info,squareArray);
+    //console.log("New CHECK: "+document.getElementById('gameMsg').innerHTML);
+    check4Tie();
+}
 
-    
+//call this function to check board state for ny ties and act accordingly
+function check4Tie() {
+    var boardState = document.getElementById('boardState').innerHTML;
+    boardState = boardState.substring(1); //remove leading comma
+    boardState = boardState.split(','); //seperate the string by commas into an array
+    var check = document.getElementById('gameMsg').innerHTML;
+    if(boardState.length >= 9 && check != "That's three in a row, Player 1 wins!" && check != "That's three in a row, Player 2 wins!") {
+        var txt1 = "Oh no! Nobody wins, it was a tie!";
+        tieSound(); //play a sound when a tie has been detected
+        writeMsg(txt1);
+        setTimeout(function() {stopGame();}, 3000);
+    }
+}
+
+//whenever a win is detected the corresponding function will
+//call this function to produce the following winning process for the game
+function winner(winDetected,winCon) {
+    if (winDetected == "win") {
+        var showme = winDetected;
+        var activePlayer = document.getElementById('showPlayer').innerHTML;
+        var txt2 = "That's three in a row, "+activePlayer+" wins!";
+        writeMsg(txt2);
+        var btn = document.getElementById('btnStart');
+        startEnabled(btn); //enable the start button
+        var btn = document.getElementById('btnStop');
+        btnDisabled(btn); //disbale the stop button
+        document.getElementById('showPlayer').innerHTML = "Game Stopped";
+        glowBoard(winCon); //call function to make the game board pulse with colors
+    }
 }
